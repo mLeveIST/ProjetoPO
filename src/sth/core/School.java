92638ea29e00b1ee3e5dc924a3cd63574fb85676
mLeveIST <miguel.leve@tecnico.ulisboa.pt>
2018-11-07 21:45:15 +0000

    Adicao de 2 metodos:  + createCourse(String) : void  + createDiscipline(String, String, int, int) : void
    Limpeza de alguns comentarios desnecessarios.

--- src/sth/core/School.java
+++ src/sth/core/School.java
@@ -10,6 +10,32 @@ public class School implements java.io.Serializable {
 	// Serial number for serialization
 	private static final long serialVersionUID = 201810051538L;
 
+	private String _name;
+	private List<Course> _courses;
+	private Map<Integer, Person> _people; // Might change this map
+
+	public School(String name) {
+		_name = name;
+		_courses = new ArrayList<>();
+		_people = new HashMap<>();
+	}
+
+	public createCourse(String name) {
+		Course course = new Course(name);
+
+		if (course != null)
+			_courses.add(course);
+	}
+
+	public void createDiscipline(String name, String course, int maxStudents, int teacher) {
+
+		for (Course c : _courses)
+			if (c.getName().equals(course))
+				if (_people.get(teacher) != null) {
+					
+				}
+	}
+
 	/**
 	 * @param filename
 	 * @throws BadEntryException
