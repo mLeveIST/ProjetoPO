0ce0434593513d68b91dd97708e024337e060f1a
Rafafigo <37385068+RafaelAlexandreIST@users.noreply.github.com>
2018-11-08 22:09:10 +0000

    Added method execute()

--- src/sth/app/person/DoShowAllPersons.java
+++ src/sth/app/person/DoShowAllPersons.java
@@ -24,7 +24,8 @@ public class DoShowAllPersons extends Command<SchoolManager> {
   /** @see pt.tecnico.po.ui.Command#execute() */
   @Override
   public final void execute() {
-    //FIXME implement command
+    _display.addLine(_receiver.showAllPersons());
+    _display.display();
   }
 
 }
