95e8cd3e08a1a6c5fb0050817cab4481311197be
mLeveIST <miguel.leve@tecnico.ulisboa.pt>
2018-11-10 18:51:22 +0000

    Worked on the sth.core.Person class:
    - Imported java.util.Comparator
    
    - Realized (implements) Comparable<Person>
    
    - Added atribute _name : String
    - Added atribute _teleNum : int
    - Added atribute _id : int
    - Added atribute _compByName : Comparator<Person>
    
    - Added constructor Person(String, int, int)
    - Added getters for all atributes
    - Added setter for _teleNum
    - Overriden toString() : String
    - Overriden equals(Object) : boolean
    _ Overriden compareTo(Person) : int
    - Overriden hashCode() : int

--- src/sth/core/Person.java
+++ src/sth/core/Person.java
@@ -1,3 +1,63 @@
 package sth.core;
 
-public abstract class Person {}
+import java.util.Comparator;
+
+public abstract class Person implements Comparable<Person> {
+
+	private String _name;
+	private int _id;
+	private int _teleNum;
+
+	// Might turn to method or declare instead at sort call outside Person
+	private Comparator<Person> _compByName = new Comparator<Person>() {
+
+		@Override
+		public int compare(Person p1, Person p2) {
+			return p1._name.compareTo(p2._name);
+		}
+	};
+
+	public Person(String name, int id, int teleNum) {
+		_name = name;
+		_id = id;
+		_teleNum = teleNum;
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public int getId() {
+		return _id;
+	}
+
+	public int getTeleNum() {
+		return _teleNum;
+	}
+
+	public Comparator<Person> getComparatorByName() {
+		return _compByName;
+	}
+
+	@Override
+	public String toString() {
+		return "|" + _id + "|" + _teleNum + "|" + _name;
+	}
+
+	@Override
+	public int compareTo(Person p) {
+		return _id - p._id;
+	}
+
+	// ******* IF A SET OF PERSONS IS USED **********
+
+	@Override
+	public boolean equals(Object o) {
+		return o != null && o instanceof Person && _id == ((Person) o)._id;
+	}
+
+	@Override
+	public int hashCode() {
+		return _id;
+	}
+}
