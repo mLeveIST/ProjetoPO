3853ec88f85b3e960937b59a00d008d2d9b7941e
mLeveIST <miguel.leve@tecnico.ulisboa.pt>
2018-11-11 19:31:52 +0000

    Worked on the sth.core.Teacher class:
    - Imported java.util.List
    - Imported java.util.ArrayList
    
    - Generalized (extends) sth.core.Person
    
    - Added atribute _courses : List<Course>
    - Added atribute _disciplines : List<Discipline>
    
    - Added constructor Teacher(int, int, String)
    - Overriden addContext(Course, Discipline)

--- src/sth/core/Teacher.java
+++ src/sth/core/Teacher.java
@@ -1,5 +1,27 @@
 package sth.core;
 
-public class Teacher {
-	
+import java.util.List;
+import java.util.ArrayList;
+
+public class Teacher extends Person {
+
+	private List<Course> _courses;
+	private List<Discipline> _disciplines;
+
+	Teacher(int id, int phoneNum, String name) {
+		super(id, phoneNum, name);
+
+		_courses = new ArrayList<>();
+		_disciplines = new ArrayList<>();
+	}
+
+	@Override
+	void addContext(Course course, Discipline discipline) {
+
+		_courses.add(course);
+		_disciplines.add(discipline);
+
+		// -!-!-!-!-!- //
+		//discipline.addTeacher(this);
+	}
 }
