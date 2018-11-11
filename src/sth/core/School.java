0e51f098a1676bff402a94ca204e8bbbf19bf6bd
mLeveIST <miguel.leve@tecnico.ulisboa.pt>
2018-11-11 19:39:26 +0000

    Worked on the sth.core.School class:
    - Added atribute _schoolParser : SchoolParser
    
    - Updated Shcool(String) constructor
    - Removed createCourse(String) : void
    - Added parseCourse(String) : Course
    - Removed createDiscipline(String, String, int, int) : void
    - Added addPerson(String, int, int, String) : Person
    - Updated importFile(String) : void
    
    Worked on the sth.core.School.SchoolParser class:
    
    - Added atribute _currPerson : Person
    
    - Added parseFile(String) : void
    - Added parseLine(String) : void
    - Added parseHeaderPerson(String) : void
    - Added parseContext(String) : void

--- src/sth/core/School.java
+++ src/sth/core/School.java
@@ -3,11 +3,15 @@ package sth.core;
 import sth.core.exception.BadEntryException;
 import sth.core.exception.NoSuchPersonIdException;
 
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.Reader;
+
 import java.util.List;
 import java.util.Map;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.io.IOException;
 
 public class School implements java.io.Serializable {
 	
@@ -16,40 +20,112 @@ public class School implements java.io.Serializable {
 
 	private String _name;
 	private List<Course> _courses;
-	private Map<Integer, Person> _people; // Might change this map
+	private Map<Integer, Person> _people;
+	private SchoolParser _schoolParser;
 
 	public School(String name) {
 		_name = name;
 		_courses = new ArrayList<>();
 		_people = new HashMap<>();
+		_schoolParser = new SchoolParser();
 	}
 
-	public void createCourse(String name) {
-		//Course course = new Course();
+	public Course parseCourse(String name) {
 
-		//if (course != null)
-		//	_courses.add(course);
+		// ------ Uncomment after Course is implemented --------
+
+		/*Course course;
+		
+		for (Course c : _courses)
+			if (c.getName().equals(name))
+				return c;
+
+		course = new Course(name);
+		_courses.add(course);
+		return course;*/
+		return null;
 	}
 
-	public void createDiscipline(String name, String course, int maxStudents, int teacher) {
+	public Person addPerson(String type, int id, int phoneNum, String name) throws BadEntryException {
+		Person person;
 
-		//for (Course c : _courses)
-		//	if (c.getName().equals(course))
-		//		if (_people.get(teacher) != null) {
-					
-		//		}
+		switch (type) {
+			case "ALUNO":
+				// -!-!-!-!-!- //
+				//person = new Student(id, phoneNum, name, false);
+				//break;
+			case "DELEGADO":
+				// -!-!-!-!-!- //
+				//person = new Student(id, phoneNum, name, true);
+				//break;
+			case "DOCENTE":
+				person = new Teacher(id, phoneNum, name);
+				break;
+			case "FUNCIONÁRIO":
+				person = new Employee(id, phoneNum, name);
+				break;
+			default:
+				throw new BadEntryException("Invalid token " + type + "in line describing a person");
+		}
+
+		_people.put(id, person);
+		return person;
 	}
 
 	/**
-	 * @param filename
+	 * @param fileName
 	 * @throws BadEntryException
 	 * @throws IOException
 	 */
-	void importFile(String filename) throws IOException, BadEntryException {
-		
+	void importFile(String fileName) throws IOException, BadEntryException {
+		_schoolParser.parseFile(fileName);
 	}
 
 	private class SchoolParser {
 
+		private Person _currPerson;
+
+		void parseFile(String fileName) throws IOException, BadEntryException {
+			String line;
+
+			try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+				while ((line = reader.readLine()) != null)
+					parseLine(line);
+			}
+		}
+
+		private void parseLine(String line) throws BadEntryException {
+			if (line.startsWith("#"))
+				parseContext(line);
+			else
+				parseHeaderPerson(line);
+		}
+
+		private void parseHeaderPerson(String header) throws BadEntryException {
+			String[] components = header.split("\\|");
+
+			if (components.length != 4)
+				throw new BadEntryException("Invalid line: " + header);
+
+			try {
+				_currPerson = addPerson(components[0], Integer.parseInt(components[1]), Integer.parseInt(components[2]), components[3]);
+			} catch (NumberFormatException nfe) {
+				throw new BadEntryException("Invalid line: " + header);
+			}
+		}
+
+		private void parseContext(String line) throws BadEntryException {
+			String[] components = line.substring(2).split("\\|");
+
+			if (components.length != 2)
+				throw new BadEntryException("Invalid line: " + line);
+
+			Course course = parseCourse(components[0]);
+			// -!-!-!-!-!- //
+			//Discipline discipline = course.parseDiscipline(components[1])
+
+			// -!-!-!-!-!- //
+			//_currPerson.addContext(course, discipline);
+		}
 	}	
 }
