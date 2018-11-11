bf72195cd580c8fa4bce4805093de0fe02deea14
Rafafigo <37385068+RafaelAlexandreIST@users.noreply.github.com>
2018-11-11 23:23:44 +0000

    Do save.java Adicionei: _fileName - nome do ficheiro fiz o execute() do doSave

--- src/sth/app/main/DoSave.java
+++ src/sth/app/main/DoSave.java
@@ -1,6 +1,9 @@
 package sth.app.main;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 import pt.tecnico.po.ui.Command;
 import pt.tecnico.po.ui.Input;
@@ -12,20 +15,40 @@ import sth.core.SchoolManager;
  * 4.1.1. Save to file under current name (if unnamed, query for name).
  */
 public class DoSave extends Command<SchoolManager> {
-  //FIXME add input fields if needed
+  Input<String> _fileName = null;
 
   /**
    * @param receiver
    */
   public DoSave(SchoolManager receiver) {
     super(Label.SAVE, receiver);
-    //FIXME initialize input fields if needed
+    _fileName = _form.addStringInput(Message.saveAs());
   }
 
   /** @see pt.tecnico.po.ui.Command#execute() */
   @Override
   public final void execute() {
-    //FIXME implement command
-  }
+    ObjectOutputStream objOut = null;
+    if(_fileName == null)
+        _form.parse();
 
+    try {
+      FileOutputStream fpout = new FileOutputStream(_fileName.value());
+      objOut = new ObjectOutputStream(fpout);
+      objOut.writeObject(_receiver.saveFile());
+
+    } catch (FileNotFoundException fnfe) {
+      _display.popup(Message.fileNotFound());
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        if (objOut != null)
+          objOut.close();
+      } catch(IOException x){
+        x.printStackTrace();
+      }
+    }
+  }
 }
+
