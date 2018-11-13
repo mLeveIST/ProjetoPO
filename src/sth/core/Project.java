bbf0dcdd7828520f2e70257d3fce8579650942c2
mLeveIST <miguel.leve@tecnico.ulisboa.pt>
2018-11-13 04:10:49 +0000

    Worked on ath.core.Project class:
    - Imported java.util.HashMap
    - Imported java.util.Map
    - Imported sth.core.exception.NoSuchProjectIdException
    
    - Realized Comparable interface
    
    - Added attribute _name : String
    - Added attribute _description : String
    - Added attribute _open : boolean
    - Added attribute _submissions : Map<Integer,Submission>
    - Added attribute _survey : Survey
    
    - Added Project(String) constructor
    - Added getters for _name, _description and _open
    - Added submit(Student, String) : void
    - Added close() : void
    - Overriden compareTo(Project) : int
    
    - Added private class Submission
    
      - Added attribute _submission
    
      - Overriden toString() : String

--- src/sth/core/Project.java
+++ src/sth/core/Project.java
@@ -1,5 +1,59 @@
 package sth.core;
 
-public class Project {
+import java.util.HashMap;
+import java.util.Map;
 
+import sth.core.exception.NoSuchProjectIdException;
+
+public class Project implements Comparable<Project> {
+
+	private String _name;
+	private String _description;
+	private boolean _open;
+	private Map<Integer, Submission> _submissions;
+	private Survey _survey;
+
+	Project(String name) {
+		_name = name;
+		_open = true;
+	}
+
+	String getName() {
+		return _name;
+	}
+
+	String getDescription() {
+		return _description;
+	}
+
+	boolean isOpen() {
+		return _open;
+	}
+
+	void submit(Student student, String submission) {
+		_submissions.put(student.getId(), new Submission(submission));
+	}
+
+	void close() {
+		_open = false;
+	}
+
+	@Override
+	public int compareTo(Project p) {
+		return _name.compareTo(p._name);
+	}
+
+	private class Submission {
+
+		private String _submission;
+
+		Submission(String submission) {
+			_submission = submission;
+		}
+
+		@Override
+		public String toString() {
+			return _submission;
+		}
+	}
 }
