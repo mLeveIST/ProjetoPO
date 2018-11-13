09f54cfd4a332489b8c19176eddc9741ca7c7d39
mLeveIST <miguel.leve@tecnico.ulisboa.pt>
2018-11-13 04:16:16 +0000

    Worked on sth.core.Teacher class:
    - Imported java.util.HashMap
    - Imported java.util.Map
    - Imported sth.core.exception.BadEntryException
    - Imported sth.core.exception.NoSuchDisciplineIdException
    - Imported sth.core.exception.NoSuchProjectIdException
    
    - Removed temporarly attribute _courses
    
    - Replaced addContext with parseContext
    - Added createProject(String, String) : void
    - Added closeProject(String, String) : void
    - Added showStudents(String) : String
    - Added getDiscipline(String) : Discipline

--- src/sth/core/Teacher.java
+++ src/sth/core/Teacher.java
@@ -1,27 +1,58 @@
 package sth.core;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import sth.core.exception.BadEntryException;
+import sth.core.exception.NoSuchDisciplineIdException;
+import sth.core.exception.NoSuchProjectIdException;
 
 public class Teacher extends Person {
 
-	private List<Course> _courses;
-	private List<Discipline> _disciplines;
+	//private List<Course> _courses;  //Might be useless
+	private Map<String, Discipline> _disciplines;
 
 	Teacher(int id, int phoneNum, String name) {
 		super(id, phoneNum, name);
 
-		_courses = new ArrayList<>();
-		_disciplines = new ArrayList<>();
+		//_courses = new ArrayList<>();
+		_disciplines = new HashMap<>();
+	}
+
+	void createProject(String disName, String projName) throws NoSuchDisciplineIdException, NoSuchProjectIdException {
+		Discipline discipline = getDiscipline(disName);
+		//!_!_!_!_!_!//discipline.createProject(projName);
+	}
+
+	void closeProject(String disName, String projName) throws NoSuchDisciplineIdException, NoSuchProjectIdException {
+		Discipline discipline = getDiscipline(disName);
+		//!_!_!_!_!_!//Project project = discipline.getProject(projName);
+		//!_!_!_!_!_!//project.close();
+	}
+
+	String showStudents(String disName) throws NoSuchDisciplineIdException {
+		Discipline discipline = getDiscipline(disName);
+		return discipline.showStudents();
+	}
+
+	private Discipline getDiscipline(String disName) throws NoSuchDisciplineIdException {
+		if (!_disciplines.containsKey(disName))
+			throw new NoSuchDisciplineIdException(disName);
+		return _disciplines.get(disName);
 	}
 
 	@Override
-	void addContext(Course course, Discipline discipline) {
+	void parseContext(String context, School school) throws BadEntryException {
+		String components[] =  context.split("\\|");
 
-		_courses.add(course);
-		_disciplines.add(discipline);
+    	if (components.length != 2)
+      		throw new BadEntryException("Invalid line context " + context);
 
-		// -!-!-!-!-!- //
-		//discipline.addTeacher(this);
+    	Course course = school.parseCourse(components[0]);
+    	Discipline discipline = course.parseDiscipline(components[1]);
+
+    	//_courses.add(course);
+    	_disciplines.put(discipline.getName(), discipline);
+    	discipline.addTeacher(this);
 	}
 }
