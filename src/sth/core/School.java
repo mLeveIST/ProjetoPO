ca53b59326ddebe620cb295adf3502822b25d92d
mLeveIST <miguel.leve@tecnico.ulisboa.pt>
2018-11-13 04:23:24 +0000

    Worked on sth.core.School class:
    - Updated importFile to new context
    - Added searchPerson(String) : String
    - Added showAllPersons() : String
    - Added getPerson(int) : Person
    
    - Handled some exceptions

--- src/sth/core/School.java
+++ src/sth/core/School.java
@@ -1,17 +1,15 @@
 package sth.core;
 
-import sth.core.exception.BadEntryException;
-import sth.core.exception.NoSuchPersonIdException;
-
 import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.Reader;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.ArrayList;
-import java.util.HashMap;
+
+import sth.core.exception.BadEntryException;
+import sth.core.exception.NoSuchPersonIdException;
 
 public class School implements java.io.Serializable {
 	
@@ -21,16 +19,55 @@ public class School implements java.io.Serializable {
 	private String _name;
 	private List<Course> _courses;
 	private Map<Integer, Person> _people;
-	private SchoolParser _schoolParser;
 
 	public School(String name) {
 		_name = name;
 		_courses = new ArrayList<>();
 		_people = new HashMap<>();
-		_schoolParser = new SchoolParser();
 	}
 
-	public Course parseCourse(String name) {
+	Person getPerson(int id) throws NoSuchPersonIdException {
+		if (!_people.containsKey(id))
+			throw new NoSuchPersonIdException(id);
+		return _people.get(id);
+	}
+
+	String showAllPersons() {
+		String info = "";
+
+		List<Person> people = (List<Person>) _people.values();
+		Collections.sort(people);
+
+		for (Person p : people)
+			info += p.toString();
+
+		return info;
+	}
+
+	String searchPerson(String name) {
+		String info = "";
+
+		List<Person> people = (List<Person>) _people.values();
+		Collections.sort(people, Person.getComparatorByName());
+
+		for (Person p : people)
+			if (p.getName().contains(name))
+				info += p.toString();
+
+		return info;
+	}
+
+	/**
+	 * @param fileName
+	 * @throws BadEntryException
+	 * @throws IOException
+	 */
+	void importFile(String fileName) throws IOException, BadEntryException {
+		Parser schoolParser = new Parser(this);
+		schoolParser.parseFile(fileName);
+	}
+
+	Course parseCourse(String name) {
 		Course course;
 		
 		for (Course c : _courses)
@@ -42,7 +79,7 @@ public class School implements java.io.Serializable {
 		return course;
 	}
 
-	public Person addPerson(String type, int id, int phoneNum, String name) throws BadEntryException {
+	Person parsePerson(String type, int id, int phoneNum, String name) throws BadEntryException {
 		Person person;
 
 		switch (type) {
@@ -62,60 +99,5 @@ public class School implements java.io.Serializable {
 
 		_people.put(id, person);
 		return person;
-	}
-
-	/**
-	 * @param fileName
-	 * @throws BadEntryException
-	 * @throws IOException
-	 */
-	void importFile(String fileName) throws IOException, BadEntryException {
-		_schoolParser.parseFile(fileName);
-	}
-
-	private class SchoolParser {
-
-		private Person _currPerson;
-
-		void parseFile(String fileName) throws IOException, BadEntryException {
-			String line;
-
-			try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-				while ((line = reader.readLine()) != null)
-					parseLine(line);
-			}
-		}
-
-		private void parseLine(String line) throws BadEntryException {
-			if (line.startsWith("#"))
-				parseContext(line);
-			else
-				parseHeaderPerson(line);
-		}
-
-		private void parseHeaderPerson(String header) throws BadEntryException {
-			String[] components = header.split("\\|");
-
-			if (components.length != 4)
-				throw new BadEntryException("Invalid line: " + header);
-
-			try {
-				_currPerson = addPerson(components[0], Integer.parseInt(components[1]), Integer.parseInt(components[2]), components[3]);
-			} catch (NumberFormatException nfe) {
-				throw new BadEntryException("Invalid line: " + header);
-			}
-		}
-
-		private void parseContext(String line) throws BadEntryException {
-			String[] components = line.substring(2).split("\\|");
-
-			if (components.length != 2)
-				throw new BadEntryException("Invalid line: " + line);
-
-			Course course = parseCourse(components[0]);
-
-			Discipline discipline = course.parseDiscipline(components[1]);
-			_currPerson.addContext(course, discipline);
-		}
 	}	
 }
