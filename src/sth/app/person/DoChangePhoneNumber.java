eb9309caf0c3b5e8e0ceef123de5bed61696f414
RafaelAlexandreIST <rafael.alexandre.roberto.figueiredo@tecnico.ulisboa.pt>
2018-11-14 16:06:30 +0000

    Tirei os imports desnecessarios e limpei os fix me

--- src/sth/app/person/DoChangePhoneNumber.java
+++ src/sth/app/person/DoChangePhoneNumber.java
@@ -1,11 +1,10 @@
 package sth.app.person;
 
 import pt.tecnico.po.ui.Command;
-import pt.tecnico.po.ui.Display;
 import pt.tecnico.po.ui.Input;
 import sth.core.SchoolManager;
 
-//FIXME import other classes if needed
+
 
 /**
  * 4.2.2. Change phone number.
